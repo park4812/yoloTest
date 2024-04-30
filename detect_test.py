3f09fd585a9c90e7bec6e7fb55a7825961665ac1
KyungDae PARK <kyungdae.park@futurereality.io>
2024-04-30 18:59:56 +0900

    x

--- detect_test.py
+++ detect_test.py
@@ -14,7 +14,7 @@ class HumanDetector:
                  img_size:int=640,
                  confidence_threshold:float=0.75,
                  iou_threshold:float=0.45,
-                 device='0',
+                 device='cpu',
                   ) -> None:
         '''
         weights: 학습된 가중치 파일 위치
@@ -34,7 +34,7 @@ class HumanDetector:
         self.iou_thres = iou_threshold
         self.agnostic_nms = True
 
-    def detect_from_files(self, index, folder_path, save_json_path: str = None, return_json=False,
+    def detect_from_files(self, folder_path, save_json_path: str = None, return_json=False,
                           save_images_path: str = None):
         '''
         folder_path: 입력할 이미지들의 폴더 위치
@@ -42,30 +42,67 @@ class HumanDetector:
         return_json: True면 json 리턴 // False면 dict 리턴
         save_images_path: 바운딩 박스가 그려진 이미지를 저장할 폴더 경로
         '''
-        max = float('-inf')
-        min = float('inf')
-        sum = 0
+        total_min = float('inf')
+        total_max = float('-inf')
+        total_sum = 0
+
+        numpy_min = float('inf')
+        numpy_max = float('-inf')
+        numpy_sum = 0
+
+        unsqueeze_min = float('inf')
+        unsqueeze_max = float('-inf')
+        unsqueeze_sum = 0
+
+        pred1_min = float('inf')
+        pred1_max = float('-inf')
+        pred1_sum = 0
+
+        pred2_min = float('inf')
+        pred2_max = float('-inf')
+        pred2_sum = 0
+
+        scale_coords_min = float('inf')
+        scale_coords_max = float('-inf')
+        scale_coords_sum = 0
+
+
         cnt = 0
+
         dataset = LoadImages(folder_path, img_size=self.img_size, stride=self.stride)
 
-        names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
-        program_start = time.time() * 1000  # Convert to milliseconds
-
         for path, img, im0s, vid_cap in dataset:
+            total_start_time = (time.time() * 1000)
 
-            start_time = (time.time() * 1000) - program_start
+            start_time = (time.time() * 1000)
             img = torch.from_numpy(img).to(self.device)
+            end_time = (time.time() * 1000)
             img = img.float()  # uint8 to fp16/32
             img /= 255.0  # 0 - 255 to 0.0 - 1.0
-            print(2)
+
+            numpy_min, numpy_max, numpy_sum = self.measure_execution_time(numpy_min, numpy_max, numpy_sum, start_time, end_time)
+
             if img.ndimension() == 3:
+                start_time = (time.time() * 1000)
                 img = img.unsqueeze(0)
+                end_time = (time.time() * 1000)
+
+                unsqueeze_min, unsqueeze_max, unsqueeze_sum = self.measure_execution_time(unsqueeze_min, unsqueeze_max, unsqueeze_sum,
+                                                                              start_time, end_time)
 
             with torch.no_grad():  # Calculating gradients would cause a GPU memory leak
+                start_time = (time.time() * 1000)
                 pred = self.model(img, augment=self.augment)[0]
+                end_time = (time.time() * 1000)
 
+                pred1_min, pred1_max, pred1_sum = self.measure_execution_time(pred1_min, pred1_max, pred1_sum,
+                                                                              start_time, end_time)
             # Apply NMS
+            start_time = (time.time() * 1000)
             pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=None, agnostic=self.agnostic_nms)
+            end_time = (time.time() * 1000)
+
+            pred2_min, pred2_max, pred2_sum = self.measure_execution_time(pred2_min, pred2_max, pred2_sum, start_time, end_time)
 
             # Process detections
             for i, det in enumerate(pred):  # detections per image
@@ -74,29 +111,44 @@ class HumanDetector:
 
                 if len(det):
                     # Rescale boxes from img_size to im0 size
+                    start_time = (time.time() * 1000)
                     det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                    end_time = (time.time() * 1000)
 
-            end_time = (time.time() * 1000) - program_start
+                    scale_coords_min, scale_coords_max, scale_coords_sum = self.measure_execution_time(scale_coords_min, scale_coords_max, scale_coords_sum,
+                                                                                  start_time, end_time)
+            total_end_time = (time.time() * 1000)
+            total_min, total_max, total_sum = self.measure_execution_time(total_min, total_max, total_sum, total_start_time, total_end_time)
 
-            execution_time = end_time - start_time
-            if execution_time > max:
-                max = execution_time
-            if execution_time < min:
-                min = execution_time
-            sum += execution_time
-        print(f"Maximum time: {max}")
-        print(f"Minimum time: {min}")
-        print(f"execution time: {sum / cnt}")
-        print(f"frame: {cnt}")
+        self.resultPrint("tatal", total_min, total_max, total_sum, cnt)
+
+        self.resultPrint("numpy", numpy_min, numpy_max, numpy_sum, cnt)
+
+        self.resultPrint("unsqueeze", unsqueeze_min, unsqueeze_max, unsqueeze_sum, cnt)
+
+        self.resultPrint("pred1", pred1_min, pred1_max, pred1_sum, cnt)
+
+        self.resultPrint("pred2", pred2_min, pred2_max, pred2_sum, cnt)
+
+        self.resultPrint("scale_coords", scale_coords_min, scale_coords_max, scale_coords_sum, cnt)
+
+        print(f"frame: {cnt}\n")
         return "0"
 
+    def resultPrint(self, name, min, max, sum, cnt):
+        print(f"\n{name} Maximum time: {max}")
+        print(f"{name} Minimum time: {min}")
+        print(f"{name} execution time: {sum / cnt}")
 
-def run_human_detector(index, folder_path, save_json_path, save_images_path):
-    human_dect = HumanDetector(weights='weights/yolov7_training.pt')
-    human_dect.detect_from_files(index, folder_path, save_json_path, save_images_path)
+    def measure_execution_time(self, min_val, max_val, sum_val, start_time, end_time):
+        execution_time = end_time - start_time
+        min_val = min(min_val, execution_time)
+        max_val = max(max_val, execution_time)
+        sum_val += execution_time
+        return min_val, max_val, sum_val
 
 if __name__ == '__main__':
 
     human_dect = HumanDetector(weights='weights/yolov7_training.pt')
-    human_dect.detect_from_files('1', folder_path='inference/videos/',save_json_path='detect_results/result.json', save_images_path='detect_results')
+    human_dect.detect_from_files(folder_path='inference/videos/',save_json_path='detect_results/result.json', save_images_path='detect_results')
     print('finished')
---
+++ t.bat
@@ -0,0 +1,4 @@
+@echo off
+call conda activate yolo
+python C:\Users\park\Documents\yolo_in_windows\detect_test.py
+pause
